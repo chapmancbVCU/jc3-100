4f8c252f934cb7aa64ff2f80c8bb37581ffaac2b
Chad Chapman <chad.chapman2010@gmail.com>
2023-08-02 18:44:44 -0400

    Scripts for copying game save files for each part of speed run.

---
+++ copy_saves/part_7.bat
@@ -0,0 +1,3 @@
+set settings_dir=76561199157138841
+
+copy "C:\Users\chadchapman\Documents\Live_Splits\jc3-100\Part 7\*.*" "C:\Users\%USERNAME%\Documents\Square Enix\Just Cause 3\Saves\%settings_dir%"
